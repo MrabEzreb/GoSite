d867ba70d2c211a7cd7c9fc089c62463aa3e4335
MrabEzreb <mrabezreb@outlook.com>
2016-06-17 21:17:45 -0400

    Added even more navbar formatting

--- deps/js/navbar.js
+++ deps/js/navbar.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var navbar = $("<span id=\"navbar\"><table><tr><td>Home</td><td>Public</td><td>Sitemap</td></tr></table></span>");
+    var navbar = $("<span id=\"navbar\"><table><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
     $("body").prepend(navbar);
-    var navbars = $("<style>#navbar {width: 100%; text-align: center;} table {border: 1px solid black;} td {border: 1px dashed black;}</style>")
+    var navbars = $("<style>#navbar {width: 100%; text-align: center;} table {border: 1px solid black;} td {border: 1px dashed black;} a {text-decoration: none;}</style>");
+    $("head").append(navbars);
 });
