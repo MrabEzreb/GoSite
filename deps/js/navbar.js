55b3a7f7342f876375ddfe0295d30aa9a98d46ba
MrabEzreb <mrabezreb@outlook.com>
2016-06-17 21:28:31 -0400

    Fixed typo

--- deps/js/navbar.js
+++ deps/js/navbar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var navbar = $("<span id=\"navbar\"><table cellspacing="0" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
+    var navbar = $("<span id=\"navbar\"><table cellspacing=\"0\" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
     $("body").prepend(navbar);
     var navbars = $("<style>#navbar {width: 100%; text-align: center;} tr {border: 1px solid black;} td {border: 1px dashed black;} a {text-decoration: none;}</style>");
     $("head").append(navbars);
