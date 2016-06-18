4ff499fc1edc3388a3791905a25bc63fbcbb11e1
MrabEzreb <mrabezreb@outlook.com>
2016-06-17 21:46:50 -0400

    added border to the navbar

--- deps/js/navbar.js
+++ deps/js/navbar.js
@@ -5,7 +5,7 @@ function navbarResize() {
 $(document).ready(function() {
     var navbar = $("<span id=\"navbar\"><table cellspacing=\"0\" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
     $("body").prepend(navbar);
-    var navbars = $("<style>#navbar {position: relative; text-align: center;} tr {border: 1px solid black;} td {padding-left: 10px; padding-right: 10px; border: 1px dashed black;} a {text-decoration: none;}</style>");
+    var navbars = $("<style>#navbar {position: relative; text-align: center;} table {border: 1px solid black;} td {padding-left: 10px; padding-right: 10px; border: 1px dashed black;} a {text-decoration: none;}</style>");
     $("head").append(navbars);
     navbarResize();
 });
