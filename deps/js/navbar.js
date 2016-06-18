64509781b3d143ae0f3eef8929de20378b427d46
MrabEzreb <mrabezreb@outlook.com>
2016-06-17 21:23:37 -0400

    Added more things to the navbar

--- deps/js/navbar.js
+++ deps/js/navbar.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-    var navbar = $("<span id=\"navbar\"><table><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
+    var navbar = $("<span id=\"navbar\"><table id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
     $("body").prepend(navbar);
-    var navbars = $("<style>#navbar {width: 100%; text-align: center;} table {border: 1px solid black;} td {border: 1px dashed black;} a {text-decoration: none;}</style>");
+    var navbars = $("<style>#navbar {width: 100%; text-align: center;} tr {border: 1px solid black;} td {border: 1px dashed black;} a {text-decoration: none;}</style>");
     $("head").append(navbars);
+    navbarResize();
 });
+
+$(window).resize(navbarResize);
+
+function navbarResize() {
+    $("#navbar").css("width", $("#navtable").width()).css("left", ($(window).width/2)-($("#navtable").width()));
+}
