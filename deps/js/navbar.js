09ce60534668433773eec902bc8aaa96923a7fa5
MrabEzreb <mrabezreb@outlook.com>
2016-06-17 21:35:29 -0400

    Fixed Alignment of the Navbar

--- deps/js/navbar.js
+++ deps/js/navbar.js
@@ -1,13 +1,13 @@
+function navbarResize() {
+    $("#navbar").css("width", $("#navtable").width()).css("left", ($(window).width()/2)-($("#navtable").width()/2));
+}
+
 $(document).ready(function() {
     var navbar = $("<span id=\"navbar\"><table cellspacing=\"0\" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
     $("body").prepend(navbar);
-    var navbars = $("<style>#navbar {width: 100%; text-align: center;} tr {border: 1px solid black;} td {border: 1px dashed black;} a {text-decoration: none;}</style>");
+    var navbars = $("<style>#navbar {width: 100%; text-align: center;} tr {border: 1px solid black;} td {padding-left: 10px; padding-right: 10px; border: 1px dashed black;} a {text-decoration: none;}</style>");
     $("head").append(navbars);
     navbarResize();
 });
 
 $(window).resize(navbarResize);
-
-function navbarResize() {
-    $("#navbar").css("width", $("#navtable").width()).css("left", ($(window).width/2)-($("#navtable").width()));
-}
