e836260d78bfe6bb5c5f9ecf74419a736705bba3
MrabEzreb <mrabezreb@outlook.com>
2016-06-19 18:55:02 -0400

    Added bootstrap-compatable jquery and did more work on the bootstrap1 page

--- dep/js/navbar.js
+++ dep/js/navbar.js
@@ -1,13 +1,80 @@
-function navbarResize() {
-    $("#navbar").css("width", $("#navtable").width()).css("left", ($(window).width()/2)-($("#navtable").width()/2));
+//Navbar.js
+
+var $ = window.$;
+
+function button(name, link) {
+    var liitem = $("<li></li>");
+    var alink = $("<a>" + name + "</a>").attr("href", link);
+    liitem.append(alink);
+    return liitem;
+}
+
+function drop(name, navElements) {
+    var liitem = $("<li></li>").addClass("dropdown");
+    var atoggle = $("<a></a>").addClass("dropdown-toggle").attr("data-toggle", "dropdown").attr("href", "#").append(name);
+    var spancaret = $("<span></span>").addClass("caret");
+    atoggle.append(spancaret);
+    liitem.append(atoggle);
+    var ulmenu = $("<ul></ul>").addClass("dropdown-menu");
+    for (var i = 0; i < navElements.length; i = i + 1) {
+        ulmenu.append(navElements[i]);
+    }
+    liitem.append(ulmenu);
+    return liitem;
+}
+
+var siteName = "MrabEzreb's Site";
+
+var account = [button("Login", "/account/login"), button("Signup", "/account/signup")];
+
+var mainNav = [button("Home", "/")];
+var mainNavR = [drop("Account", account)];
+
+function getNavData(navbar) {
+    var name = navbar.attr("array");
+    var active = navbar.attr("active");
+    if (name === "main") {
+        return mainNav;
+    }
+}
+
+function getNavDataR(navbar) {
+    var name = navbar.attr("arrayR");
+    if (name === "main") {
+        return mainNavR;
+    }
+}
+
+function buildItems(data, active) {
+    var items = "";
+    for (i = 0; i < data.length; i = i + 1) {
+        if (data[i].find("a").attr("href") === active) {
+            data[i].addClass("active");
+        }
+        items = items + data[i].prop("outerHTML");
+//        window.alert(data[i].prop("outerHTML"));
+    }
+    return items;
+}
+
+function insertNavbar() {
+    var navbar = $("navbar");
+//    window.alert(navbar.attr("array"));
+    var navData = getNavData(navbar);
+//    window.alert(navbar.attr("array"));
+    var navDataR = getNavDataR(navbar);
+//    window.alert(navbar.attr("array"));
+    var listItems = buildItems(navData, navbar.attr("active"));
+//    window.alert(navbar.attr("array"));
+    var listItemsR = buildItems(navDataR, "");
+//    window.alert(navbar.attr("array"));
+    var nav = $("<nav class=\"navbar navbar-default\">\r\n  <div class=\"container-fluid\">\r\n    <div class=\"navbar-header\">\r\n      <a class=\"navbar-brand\" href=\"#\">" + siteName + "<\/a>\r\n    <\/div>\r\n    <ul class=\"nav navbar-nav\">\r\n" + listItems + "    <\/ul>\r\n    <ul class=\"nav navbar-nav navbar-right\">\r\n" + listItemsR +"    <\/ul>\n\r  <\/div>\r\n<\/nav>");
+    navbar.append(nav);
+//    window.alert(navbar.attr("array"));
 }
 
 $(document).ready(function() {
-    var navbar = $("<span id=\"navbar\"><table cellspacing=\"0\" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
-    $("body").prepend(navbar);
-    var navbars = $("<style>#navbar {position: relative; text-align: center;} table {border: 1px solid black;} td {padding-left: 10px; padding-right: 10px; border: 1px dashed black;} a {text-decoration: none;}</style>");
-    $("head").append(navbars);
-    navbarResize();
+//    window.alert("hello");
+    insertNavbar();
+//    window.alert("hello");
 });
-
-$(window).resize(navbarResize);
---
+++ dep/js/old_navbar.js
@@ -0,0 +1,13 @@
+function navbarResize() {
+    $("#navbar").css("width", $("#navtable").width()).css("left", ($(window).width()/2)-($("#navtable").width()/2));
+}
+
+$(document).ready(function() {
+    var navbar = $("<span id=\"navbar\"><table cellspacing=\"0\" id=\"navtable\"><tr><td><a href=\"/\">Home</a></td><td><a href=\"/public\">Public</a></td><td><a href=\"/sitemap\">Sitemap</a></td></tr></table></span>");
+    $("body").prepend(navbar);
+    var navbars = $("<style>#navbar {position: relative; text-align: center;} table {border: 1px solid black;} td {padding-left: 10px; padding-right: 10px; border: 1px dashed black;} a {text-decoration: none;}</style>");
+    $("head").append(navbars);
+    navbarResize();
+});
+
+$(window).resize(navbarResize);
